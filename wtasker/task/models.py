2cfa949c0210d3e56fb121cf9c3933de36d5d808
wlove <lalakasuper2@gmail.com>
2024-09-13 23:16:55 +0700

    Добавил модель задач.

--- wtasker/task/models.py
+++ wtasker/task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=100, verbose_name="Заголовок")
+    description = models.TextField(verbose_name="Описание")
+    is_done = models.BooleanField(default=False, verbose_name="Выполнена")
+    due_date = models.DateField(verbose_name="Дедлайн")
+    in_backlog = models.BooleanField(default=True, verbose_name="В бэклоге")
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
